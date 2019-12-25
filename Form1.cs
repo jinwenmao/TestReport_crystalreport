16367e5e58912cce0b446166fd28a668a51db4c5
jinwenmao <jinwenmao@139.com>
2019-12-25 16:54:21 +0800

    ReportViewer不连接数据库，自定义DataSet导出到报表 from https://www.cnblogs.com/bfyx/archive/2012/12/17/2821264.html

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace TestReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataSet1 ds = new DataSet1();
+            
+            ds.mytable.AddmytableRow("a", "b", "c");
+            ds.mytable.AddmytableRow("a", "b", "c");
+            ds.mytable.AddmytableRow("a", "b", "c");
+            ds.mytable.AddmytableRow("a", "b", "c");
+
+            ReportDocument rDoc1 = new ReportDocument();     
+            //DataGridview中的datatable复制到指定的指定DataTable中，为水晶报表提供          
+               
+            //绑定水晶报表         
+            CrystalReport1 cry = new CrystalReport1();      
+            cry.Refresh();          
+            cry.SetDataSource(ds.Tables[0]);     
+            this.crystalReportViewer1.ReportSource = null;      
+            this.crystalReportViewer1.ReportSource = cry;         
+            this.crystalReportViewer1.RefreshReport();
+
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ doc/readme.txt
@@ -0,0 +1,2 @@
+https://www.cnblogs.com/bfyx/archive/2012/12/15/2819057.html
+CrystalReport不连接数据库，自定义DataSet导出到水晶报表
